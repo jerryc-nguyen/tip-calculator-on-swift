48bca02289523b49259d798824e38f98b9b5246a
Nhan Nguyen <nhannv.fu6b@gmail.com>
2016-05-21 13:39:19 +0700

    Add handle for tips changed

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func onChanged(sender: AnyObject) {
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount % 0.2
+        let total = billAmount + tip
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+        
+    }
 
+    @IBAction func onTapped(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
 }
 
